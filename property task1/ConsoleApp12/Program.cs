17116c60aeb2f27b4882fb8e3c0476fc00fb6abd
orxanmvv <m.orxan.m123@gmail.com>
2024-03-20 03:11:55 +0400

    5ci commit

---
+++ property task1/ConsoleApp12/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace ConsoleApp12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student("orxan", "Musayev", -25, -90);
+            student1.ShowInfo();
+            
+        }
+    }
+
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("deyer uygun deyil");
+                }
+            }
+        }
+
+        public Person(string name, string surname, int age)
+        {
+            Name = name;
+            Surname = surname;
+            Age = age;
+        }
+    }
+
+    public class Student : Person
+    {
+        public string StudentId { get; private set; }
+        private int _point;
+        public int Point
+        {
+            get { return _point; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    _point = value;
+                }
+                else
+                {
+                    Console.WriteLine("deyer yanlisdir");
+                }
+            }
+        }
+
+        public Student(string name, string surname, int age, int point) : base(name, surname, age)
+        {
+            Point = point;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Name: {Name}, Surname: {Surname}, Age: {Age}, StudentId: {StudentId}, Point: {Point}");
+        }
+    }
+}
---
+++ property task2/ConsoleApp13/Program.cs
@@ -0,0 +1,97 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Doctor doctor1 = new Doctor("orxan", "msuayev", "kisi", "urek hekimi", -15, -36);
+            doctor1.ShowInfo();
+
+
+
+        }
+    }
+
+    public class Human
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Gender { get; set; }
+        private int _age;
+        public int Age
+        {
+            get { return _age; }
+            set
+            {
+                if (value > 0 && value < 100)
+                {
+                    _age = value;
+                }
+                else
+                {
+                    Console.WriteLine("Age yanlis daxil edilib");
+                }
+            }
+        }
+        public Human(string name, string surname, string gender, int age)
+        {
+
+            Name = name;
+            Surname = surname;
+            Gender = gender;
+            Age = age;
+
+
+        }
+        public string GetInfo()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Gender: {Gender}, Age: {Age}";
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(GetInfo());
+        }
+    }
+
+    public class Doctor : Human
+    {  
+
+
+
+        public string Speciality { get; set; }
+        private int _experience;
+        public int Experience
+        {
+            get { return _experience; }
+            set
+            {
+                if (value >= 0)
+                {
+                    _experience = value;
+                }
+                else
+                {
+                    Console.WriteLine("Experience yanlis daxil edilib");
+                }
+            }
+        }
+        public Doctor( string name, string surname, string gender,string speciality, int experience, int age):base(name,surname,gender,age)
+        {
+            Speciality = speciality;
+            Experience = experience;
+            
+        }   
+
+        public  string GetInfo()
+        {
+            return $"Name: {Name}, Surname: {Surname}, Gender: {Gender} , Speciality:{Speciality}";
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine(GetInfo());
+        }
+    }
+
+}
